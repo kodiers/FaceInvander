829814a683ddcc00ffdf1a745273b6049bc7dead
Victor Yamchinov <kodiers@gmail.com>
2018-12-21 05:28:30 +0300

    added fire and createTarget

--- FaceInvander/ViewController.swift
+++ FaceInvander/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     let phone = SCNNode(geometry: SCNPlane(width: 1, height: 1))
     let smoothingAmount = 20
     var eyeLookHistory = ArraySlice<CGPoint>()
+    var targets = [UIImageView]()
+    var currentTarget = 0
+    var gunshot: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +33,46 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         face.addChildNode(rightEye)
         sceneView.session.delegate = self
         sceneView.scene.rootNode.addChildNode(phone)
+        // create master stack view to handle the over stack views
+        let rowStackView = UIStackView()
+        rowStackView.translatesAutoresizingMaskIntoConstraints = false
+        // force the inner stack views to space themselves equally
+        rowStackView.axis = .vertical
+        // and apply 20 points between them
+        rowStackView.spacing = 20
+        for _ in 1...6 {
+            // create six columns of stack views
+            let colStackView = UIStackView()
+            colStackView.translatesAutoresizingMaskIntoConstraints = false
+            // also equally filling and 20 points of space
+            colStackView.distribution = .fillEqually
+            colStackView.spacing = 20
+            // horizontal layout
+            colStackView.axis = .horizontal
+            // add col stack views to master row stack view
+            rowStackView.addArrangedSubview(colStackView)
+            for _ in 1...4 {
+                // inside each column add four images
+                let imageView = UIImageView(image: UIImage(named: "target"))
+                // make it fit inside
+                imageView.contentMode = .scaleAspectFit
+                imageView.alpha = 0
+                targets.append(imageView)
+                // add it to current column
+                colStackView.addArrangedSubview(imageView)
+            }
+        }
+        
+        view.addSubview(rowStackView)
+        NSLayoutConstraint.activate([
+            rowStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            rowStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            rowStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            rowStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        view.bringSubviewToFront(reticule)
+        targets.shuffle()
+        perform(#selector(createTarget), with: nil, afterDelay: 2)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -60,6 +103,13 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 */
     
     func update(using anchor: ARFaceAnchor) {
+        if let leftBlink = anchor.blendShapes[.eyeBlinkLeft] as? Float, let rightBlink = anchor.blendShapes[.eyeBlinkRight] as? Float {
+            // if both eyes simlatenously blinking more than 10% - fire and exit
+            if leftBlink > 0.1 && rightBlink > 0.1 {
+                fire()
+                return
+            }
+        }
         leftEye.simdTransform = anchor.leftEyeTransform
         rightEye.simdTransform = anchor.rightEyeTransform
         let points = [leftEye, rightEye].compactMap {eye -> CGPoint? in
@@ -93,6 +143,47 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
             return
         }
         phone.simdTransform = pov
+    }
+    
+    @objc func createTarget() {
+        // pick target
+        let target = targets[currentTarget]
+        // scale it
+        target.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+        // animate it back with 0.3 seconds and make it visible
+        UIView.animate(withDuration: 0.3) {
+            target.transform = .identity
+            target.alpha = 1
+        }
+        // move to next target
+        currentTarget += 1
+    }
+    
+    func fire() {
+        let reticuleFrame = reticule.superview?.convert(reticule.frame, to: nil)
+        // create new array by filtering our targets
+        let hitTargets = targets.filter { (imageView) -> Bool in
+            if imageView.alpha == 0 {
+                return false
+            }
+            // convert image view frame to absolute coordinates
+            let ourFrame = imageView.superview?.convert(imageView.frame, to: nil)
+            // add this if overlap the reticule
+            return (ourFrame?.intersects(reticuleFrame!))!
+        }
+        // pull out the first target if we have one
+        guard let selected = hitTargets.first else {
+            return
+        }
+        // hide that target
+        selected.alpha = 0
+        // play gun sound
+        if let url = Bundle.main.url(forResource: "shot", withExtension: "wav") {
+            gunshot = try? AVAudioPlayer(contentsOf: url)
+            gunshot?.play()
+        }
+        // create another target
+        perform(#selector(createTarget), with: nil, afterDelay: 1)
     }
     
 }
